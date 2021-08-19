77298c64a7740658f43551589a4eef5ce2b3ff11
Nitin Soni <nitinsoni3007@gmail.com>
2021-08-19 17:58:12 +0530

    Base template for app is added

---
+++ WetherForecast/WetherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WetherForecast
+//
+//  Created by Nitin Soni on 19/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
